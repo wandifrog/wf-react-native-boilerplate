3a2a6b947d343ca21a930a59330aca1a48799c9a
Wandi <wandi.wandi@salt.co.id>
2021-09-27 20:20:25 +0700

    chore: add new navigation routes

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from './screens/Home.screen'
-import ProfileScreen from './screens/Profile.screen'
 import CreateEventScreen from './screens/CreateEvent.screen'
 import EventDetailScreen from './screens/EventDetail.screen'
+import HomeScreen from './screens/Home.screen'
+import LoginScreen from './screens/Login.screen'
+import ProfileScreen from './screens/Profile.screen'
 
 const RootStack = createNativeStackNavigator()
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <RootStack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <RootStack.Screen name="Home" component={HomeScreen} />
+      <RootStack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="CreateEvent" component={CreateEventScreen} />
         <RootStack.Screen name="EventDetail" component={EventDetailScreen} />
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Login" component={LoginScreen} />
         <RootStack.Screen name="Profile" component={ProfileScreen} />
       </RootStack.Navigator>
     </NavigationContainer>
